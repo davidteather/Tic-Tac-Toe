2e32e4d5d39d0682cb78e2df7915c909cce427bf
davidteather <34144122+davidteather@users.noreply.github.com>
2018-07-21 12:40:52 -0500

    Initial commit of GUI

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,25 +1,27 @@
+from tkinter import *
+import tkinter as tk
 import os
 import time
 import random
 import sys
-print("Python Tic-Tac-Toe" + os.linesep)
-print("GitHub: https://github.com/davidteather/Tic-Tac-Toe" + os.linesep)
+from tkinter.ttk import Separator, Style
 
+window = tk.Tk()
+window.title("Tic-Tac-Toe")
+#frame = Frame(window, width=200, height=250)
+squareList = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-def printBoard(squareList):
-    # Function Prints the board
-    str(squareList)
-    print("     |     |     ")
-    print("  " + str(squareList[0]) + "  |  " + str(squareList[1]) + "  |  " + str(squareList[2]) + "  ")
-    print("     |     |     ")
-    print("-----------------")
-    print("     |     |     ")
-    print("  " + str(squareList[3]) + "  |  " + str(squareList[4]) + "  |  " + str(squareList[5]) + "  ")
-    print("     |     |     ")
-    print("-----------------")
-    print("     |     |     ")
-    print("  " + str(squareList[6]) + "  |  " + str(squareList[7]) + "  |  " + str(squareList[8]) + "  ")
-    print("     |     |     " + os.linesep)
+tile0 = StringVar()
+tile1 = StringVar()
+tile2 = StringVar()
+tile3 = StringVar()
+tile4 = StringVar()
+tile5 = StringVar()
+tile6 = StringVar()
+tile7 = StringVar()
+tile8 = StringVar()
+msg = StringVar()
+msg.set("Hello")
 
 
 def check_win(squareList):
@@ -29,17 +31,17 @@ def check_win(squareList):
     for a in winconditions:
         if len(a) == 3:
             if squareList[a[0]] == "X" and squareList[a[1]] == "X" and squareList[a[2]] == "X":
-                return 1
+                msg.set("You have won!")
             elif squareList[a[0]] == "O" and squareList[a[1]] == "O" and squareList[a[2]] == "O":
-                return 2
+                msg.set("The bot has won!")
             elif (squareList[0] != 1 and squareList[1] != 2 and squareList[2] != 3 and squareList[3] != 4
                   and squareList[4] != 5 and squareList[5] != 6 and squareList[6] != 7
                   and squareList[7] != 8 and squareList[8] != 9):
-                print(" " + os.linesep)
-                print(squareList[0])
-                return 3
+                msg.set("It is a tie!")
     else:
         return 0
+
+
 def bot_place(squareList):
     # Function to deal with the bot AI
     # Bot Move suggestions ex: [0, 1, 2] means if tiles 0 and 1 are filled then the bot will go place at tile 2
@@ -48,11 +50,11 @@ def bot_place(squareList):
               [0, 6, 3], [1, 7, 4], [2, 8, 5], [0, 2, 1], [3, 5, 4], [6, 8, 7], [0, 8, 4], [2, 6, 4]]
     for a in moveAI:
         if len(a) == 3:
-            if squareList[a[0]] == "X" and squareList[a[1]] == "X" and squareList[a[2]] != "X" \
+            if squareList[a[0]] == "O" and squareList[a[1]] == "O" and squareList[a[2]] != "X" \
                     and squareList[a[2]] != "O":
                 place = a[2]
                 return place
-            elif squareList[a[0]] == "O" and squareList[a[1]] == "O" and squareList[a[2]] != "X" \
+            elif squareList[a[0]] == "X" and squareList[a[1]] == "X" and squareList[a[2]] != "X" \
                     and squareList[a[2]] != "O":
                 place = a[2]
                 return place
@@ -60,6 +62,7 @@ def bot_place(squareList):
         # If no sequence is matched calls the bot random function
         place = bot_random(squareList)
         return place
+
 
 def bot_random(squareList):
     # bot random tile number returns tile number
@@ -71,79 +74,70 @@ def bot_random(squareList):
             continue
 
 
-def usr_placeinput(squareList):
-    # Deals with user placement and because of sloppy code everything else
-    while True:
-        try:
-            # Asks for tile input
-            place = int(input("Where would you want to place your X?" + os.linesep))
-            # Verifies that the tile exists
-            if 0 < place < 10:
-                # Subtracts one from input because indexing starts at 0 not 1
-                place -= 1
-                # Checks if there is any tiles already placed there
-                if squareList[place] != "X" and squareList[place] != "O":
-                    squareList[place] = "X"
-                    # Calls check_win and returns value
-                    iswin = check_win(squareList)
-                    if iswin == 0:
-                        bval = bot_place(squareList)
-                        squareList[bval] = "O"
-                    iswin = check_win(squareList)
-                    printBoard(squareList)
-                    # Deals with what the check_win function returns
-                    if iswin == 1:
-                        print("You have won!" + os.linesep)
-                    elif iswin == 2:
-                        print("The Bot has won!" + os.linesep)
-                    elif iswin == 3:
-                        print("You have managed to tie against... a bot. Good Job!")
-                    if iswin != 0:
-                        # Asks if user would like to play again
-                        val = input("Would you like to play again y | n" + os.linesep).lower()
-                        if val == "y":
-                            print("Resetting..." +  os.linesep)
-                            time.sleep(1.5)
-                            # Resets list to original value and reprints board
-                            squareList = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-                            # Loops back to beginning of the game
-                            printBoard(squareList)
-                            continue
-                        elif val == "n":
-                            # If user doesn't want to play again exit
-                            print("Good Bye.")
-                            time.sleep(.5)
-                            print("Exiting...")
-                            time.sleep(1.5)
-                            sys.exit()
-                        else:
-                            # If not a valid input resets anyways because yeah.
-                            print("Not a valid input")
-                            time.sleep(.5)
-                            print("Resetting Anyways...")
-                            time.sleep(1)
-                            squareList = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-                            printBoard(squareList)
-                            continue
-                elif squareList[place] == "X" or squareList[place] == "O":
-                    # Says the place is already taken
-                    print("That place is already taken." + os.linesep)
-                    printBoard(squares)
-                    continue
-            else:
-                print("That is not a valid tile." + os.linesep + "If you believe this is an error make an issue report on https://github.com/davidteather/Tic-Tac-Toe" + os.linesep)
-                continue
-        except ValueError:
-            print("That is not a valid integer." + os.linesep + "If you believe this is an error make an issue report on https://github.com/davidteather/Tic-Tac-Toe" + os.linesep)
-            continue
-        else:
-            continue
+def usrPlace(event, arg, squareList):
+    if squareList[arg] != "X" and squareList[arg] != "O":
+        squareList[arg] = "X"
+        iswin = check_win(squareList)
+        if iswin == 0:
+            place = bot_place(squareList)
+            squareList[place] = "O"
+            check_win(squareList)
+        updateBoard(squareList)
+    else:
+        msg.set("That tile is already taken.")
+
+        
+def updateBoard(squareList):
+    tile0.set(str(squareList[0]))
+    tile1.set(str(squareList[1]))
+    tile2.set(str(squareList[2]))
+    tile3.set(str(squareList[3]))
+    tile4.set(str(squareList[4]))
+    tile5.set(str(squareList[5]))
+    tile6.set(str(squareList[6]))
+    tile7.set(str(squareList[7]))
+    tile8.set(str(squareList[8]))
 
 
-# Sets the tiles to be equal to begin with
-squares = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-while True:
-    # Starts the game
-    printBoard(squares)
-    placement = usr_placeinput(squares)
 
+updateBoard(squareList)
+Label(textvariable=msg, font=("Helvetica", 20)).grid(column=1, row=0, padx=15, pady=15)
+
+t0 = Label(textvariable=tile0, font=("Helvetica", 40))
+t0.bind("<Button-1>", lambda event, squareList=squareList, arg=0: usrPlace(event, arg, squareList))
+t0.grid(column=0, row=1, padx=15, pady=15)
+
+t1 = Label(textvariable=tile1, font=("Helvetica", 40))
+t1.bind("<Button-1>", lambda event, squareList=squareList, arg=1: usrPlace(event, arg, squareList))
+t1.grid(column=1, row=1, padx=15, pady=15)
+
+t2 = Label(textvariable=tile2, font=("Helvetica", 40))
+t2.bind("<Button-1>", lambda event, squareList=squareList, arg=2: usrPlace(event, arg, squareList))
+t2.grid(column=2, row=1, padx=15, pady=15)
+
+t3 = Label(textvariable=tile3, font=("Helvetica", 40))
+t3.bind("<Button-1>", lambda event, squareList=squareList, arg=3: usrPlace(event, arg, squareList))
+t3.grid(column=0, row=2, padx=15, pady=15)
+
+t4 = Label(textvariable=tile4, font=("Helvetica", 40))
+t4.bind("<Button-1>", lambda event, squareList=squareList, arg=4: usrPlace(event, arg, squareList))
+t4.grid(column=1, row=2, padx=15, pady=15)
+
+t5 = Label(textvariable=tile5, font=("Helvetica", 40))
+t5.bind("<Button-1>", lambda event, squareList=squareList, arg=5: usrPlace(event, arg, squareList))
+t5.grid(column=2, row=2, padx=15, pady=15)
+
+t6 = Label(textvariable=tile6, font=("Helvetica", 40))
+t6.bind("<Button-1>", lambda event, squareList=squareList, arg=6: usrPlace(event, arg, squareList))
+t6.grid(column=0, row=3, padx=15, pady=15)
+
+t7 = Label(textvariable=tile7, font=("Helvetica", 40))
+t7.bind("<Button-1>", lambda event, squareList=squareList, arg=7: usrPlace(event, arg, squareList))
+t7.grid(column=1, row=3, padx=15, pady=15)
+
+t8 = Label(textvariable=tile8, font=("Helvetica", 40))
+t8.bind("<Button-1>", lambda event, squareList=squareList, arg=8: usrPlace(event, arg, squareList))
+t8.grid(column=2, row=3, padx=15, pady=15)
+
+
+window.mainloop()
